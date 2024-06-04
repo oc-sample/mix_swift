fb4f1ce8a42aa5fc17dcaa5f484b32b1b94fd255
mjzheng <mjzheng@tencent.com>
2024-06-04 20:17:16 +0800

    update

---
+++ mix_demo/mix_demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mix_demo
+//
+//  Created by mjzheng on 2024/6/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mix_demo/mix_demo/mix_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  mix_demoApp.swift
+//  mix_demo
+//
+//  Created by mjzheng on 2024/6/4.
+//
+
+import SwiftUI
+
+@main
+struct mix_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
